e5c2434cac8cc5fd3b0b3da1740757d038c1df65
crezix <52518140+crezix@users.noreply.github.com>
2021-05-26 21:19:00 +0530

    API Int

--- lib/DataPages/WaterLevel.dart
+++ lib/DataPages/WaterLevel.dart
@@ -2,40 +2,59 @@ import 'package:flutter/material.dart';
 import 'package:water_level_indicator/Screens/Menu.dart';
 
 class WaterLevel extends StatelessWidget {
-  static const String idScreen = "waterlevel" ;
+  static const String idScreen = "waterlevel";
+
   @override
   Widget build(BuildContext context) {
+    var args = ModalRoute.of(context).settings.arguments;
+
     return Scaffold(
       backgroundColor: Colors.cyan[800],
       body: Padding(
-        padding: EdgeInsets.all(6.0),child: Column(
-        children: [
-          SizedBox(height: 100,),
-          Text("Water Level", style: TextStyle(fontSize: 24.0, fontFamily: "Brand Bold" ),textAlign: TextAlign.center,),
-          SizedBox(height: 500.0,),
-          RaisedButton(
-              color: Colors.white70,
-              textColor: Colors.black,
-              child: Container(
-                height: 50.0,
-                child: Center(
-                  child: Text("Back To Main Menu", style: TextStyle(height:1.0 ,color: Colors.black, fontFamily: "Brand Bold", fontSize: 20.0),
+        padding: EdgeInsets.all(6.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 100,
+            ),
+            Text(
+              "Water Level",
+              style: TextStyle(fontSize: 24.0, fontFamily: "Brand Bold"),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              args.toString(),
+              style: TextStyle(fontSize: 24.0, fontFamily: "Brand Bold"),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 500.0,
+            ),
+            RaisedButton(
+                color: Colors.white70,
+                textColor: Colors.black,
+                child: Container(
+                  height: 50.0,
+                  child: Center(
+                    child: Text(
+                      "Back To Main Menu",
+                      style: TextStyle(
+                          height: 1.0,
+                          color: Colors.black,
+                          fontFamily: "Brand Bold",
+                          fontSize: 20.0),
+                    ),
                   ),
-
                 ),
-              ),
-              shape: new RoundedRectangleBorder(
-                borderRadius: new BorderRadius.circular(2.0),
-
-              ),
-              onPressed: ()
-              {
-                Navigator.pushNamedAndRemoveUntil(context, MenuScreen.idScreen, (route) => false);
-              }
-          ),
-        ],
-
-      ),
+                shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(2.0),
+                ),
+                onPressed: () {
+                  Navigator.pushNamedAndRemoveUntil(
+                      context, MenuScreen.idScreen, (route) => false);
+                }),
+          ],
+        ),
       ),
     );
   }
